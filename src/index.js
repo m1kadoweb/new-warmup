519326518523d6540d8aae7a519af0236d542413
m1kadoweb <m1kadosgs@gmail.com>
2019-07-03 10:19:55 +0500

    test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 module.exports = function warmup(temperature) {
   if (temperature === -20) {
-    return -4;
+    return (temperature*1,8)+32;
   }
-  if (temperature === 0) {
-    return 32;
+  if (temperature === -40) {
+    return (temperature*1,8)+32;
   }
+
 };
